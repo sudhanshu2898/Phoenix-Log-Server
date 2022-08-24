2a5452ae7cd39bceb1c289c67c10d68bc3e6ec15
Sudhanshu Jha <55803673+sudhanshu2898@users.noreply.github.com>
2022-08-24 15:18:15 +0530

    Add files via upload

--- services/messagingQueue.hpp
+++ services/messagingQueue.hpp
@@ -9,16 +9,20 @@
 class MessagingQueue{
     
     private:
-        std::queue<std::pair<std::string, std::string>> currentQueue;
         LogGenerator logGenerator;
-        /**
-         * In Pair First value is service Name, Second is JSON String
-         */
+        std::map<std::string, std::string> prevBytes;
+        std::queue<std::pair<std::string, std::string>> currentQueue;
+        /* In Pair First value is service Name, Second is JSON String. */
     
     public:
 
         void pushToQueue(std::string service, std::string message){
-            
+            if(prevBytes.find(service) != prevBytes.end()){
+                if(message[0] != '{'){
+                    message = prevBytes[service]+message;
+                }
+                prevBytes.erase(service);
+            }
             int position = 0;  
             std::string token;
             while ((position = message.find ("}{")) != std::string::npos){  
@@ -26,7 +30,11 @@ class MessagingQueue{
                 currentQueue.push({service, token});
                 message.erase(0, position + 1);
             }
-            currentQueue.push({service, message});  
+            if(message[0] == '{' && message[message.length()-1] == '}'){            
+                currentQueue.push({service, message});
+            }else{
+                prevBytes[service] = message;
+            }
         }
 
 
